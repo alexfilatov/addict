661b7b5df4a400b5ad6dbdd03199ff6159f119c6
Alex Filatov <alex@alexfilatov.com>
2016-08-16 17:14:43 +0300

    tiny test fix

--- test/presenter_test.exs
+++ test/presenter_test.exs
@@ -1,3 +1,22 @@
+
+defmodule TestAddictDrugsSchema do
+  use Ecto.Schema
+  schema "drugs" do
+    field :name, :string
+  end
+end
+
+defmodule TestAddictUserAssociationsSchema do
+  use Ecto.Schema
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :encrypted_password, :string
+    has_many :drugs, TestAddictDrugsSchema
+  end
+end
+
 defmodule PresenterTest do
   alias Addict.Presenter
   use ExUnit.Case, async: true
@@ -16,23 +35,5 @@ defmodule PresenterTest do
     assert Map.has_key?(model, :__struct__) == false
     assert Map.has_key?(model, :__meta__) == false
     assert Map.has_key?(model, :drugs) == false
-  end
-end
-
-defmodule TestAddictDrugsSchema do
-  use Ecto.Schema
-  schema "drugs" do
-    field :name, :string
-  end
-end
-
-defmodule TestAddictUserAssociationsSchema do
-  use Ecto.Schema
-
-  schema "users" do
-    field :name, :string
-    field :email, :string
-    field :encrypted_password, :string
-    has_many :drugs, TestAddictDrugsSchema
   end
 end
